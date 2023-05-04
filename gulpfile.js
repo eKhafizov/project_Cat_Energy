5de2b68e60c1d2ba1a1bb2f61c4a3ecd4b86a4f6
Evgeniy Khafizov <evg.haf@gmail.com>
2023-05-04 20:06:53 +0300

    fixed after unsuccessful building attempt

--- gulpfile.js
+++ gulpfile.js
@@ -2,45 +2,42 @@ const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
-const rename = require("gulp-rename");
-const htmlmin = require("gulp-htmlmin");
-const squoosh = require("gulp-libsquoosh");
 const postcss = require("gulp-postcss");
-const postcss_csso = require("postcss-scco");
 const autoprefixer = require("autoprefixer");
-const svgstore = require("gulp-svgstore");
-const terser = require("gulp-terser");
-const webp = require("gulp-webp");
-const del = require("del");
 const sync = require("browser-sync").create();
 
 // Styles
+
+
+
 const styles = () => {
-  return gulp
-    .src("source/sass/style.scss")
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([autoprefixer()]))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
-};
+}
 
 exports.styles = styles;
 
 // Server
+
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: "source",
+      baseDir: 'source'
     },
     cors: true,
     notify: false,
     ui: false,
   });
   done();
-};
+}
 
 exports.server = server;
 
@@ -49,6 +46,8 @@ exports.server = server;
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
-};
+}
 
-exports.default = gulp.series(styles, server, watcher);
+exports.default = gulp.series(
+  styles, server, watcher
+);
