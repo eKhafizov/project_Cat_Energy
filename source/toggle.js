07071751f2d1203ac5376d025a0053a4672498be
Evgeniy Khafizov <evg.haf@gmail.com>
2023-05-04 18:20:44 +0300

    коммит после сломанной сборки

---
+++ source/toggle.js
@@ -0,0 +1,14 @@
+<script>
+      var navMain = document.querySelector('.navigation');
+      var navToggle = document.querySelector('.navigation__button');
+      navMain.classList.remove('navigation--nojs');
+      navToggle.addEventListener('click', function() {
+        if (navMain.classList.contains('navigation--closed')) {
+          navMain.classList.remove('navigation--closed');
+          navMain.classList.add('navigation--opened');
+        } else {
+          navMain.classList.add('navigation--closed');
+          navMain.classList.remove('navigation--opened');
+        }
+      });
+    </script>
