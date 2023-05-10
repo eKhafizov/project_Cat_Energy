d339b2e678ca54f192e168cef636704820994478
Evgeniy Khafizov <evg.haf@gmail.com>
2023-05-10 21:59:40 +0300

    wrote condition states for index page

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,12 @@
-
-
-      var navMain = document.querySelector(".navigation");
-      var navToggle = document.querySelector(".navigation__button");
-      navMain.classList.remove("navigation--nojs");
-      navToggle.addEventListener("click", function () {
-        if (navMain.classList.contains("navigation--closed")) {
-          navMain.classList.remove("navigation--closed");
-          navMain.classList.add("navigation--opened");
-        } else {
-          navMain.classList.add("navigation--closed");
-          navMain.classList.remove("navigation--opened");
-        }
-      });
-
-
+var navMain = document.querySelector(".navigation");
+var navToggle = document.querySelector(".navigation__button");
+navMain.classList.remove("navigation--nojs");
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("navigation--closed")) {
+    navMain.classList.remove("navigation--closed");
+    navMain.classList.add("navigation--opened");
+  } else {
+    navMain.classList.add("navigation--closed");
+    navMain.classList.remove("navigation--opened");
+  }
+});
